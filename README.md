b56a0083f47157c6dea98292add444f0027e45d1
thunder <thunder@debianserver.thunderace.fr>
2015-03-03 16:18:48 +0100

    cleanup - screenshot - doc

--- README.md
+++ README.md
@@ -1,21 +1,22 @@
-# node-red-node-newtifrypro
-
+node-red-node-newtifrypro
+=========================
 A <a href="http://nodered.org" target="_new">Node-RED</a> node to send messages via <a href="https://play.google.com/store/apps/details?id=com.newtifry.pro">NewtifryPro</a>.
 
-Install
--------
+![screenshot](https://github.com/njh/node-red-osc/raw/master/screenshot.png)
+
+Installation
+------------
 
 Run the following command in the root directory of your Node-RED install (Not yet : work in progress)
 
     npm install node-red-newtifrypro
 
-
 Usage
 -----
-
 Uses NewtifryPro (NP) to push the **msg.message** and **msg.title** to an Android device that has NewtifryPro Android app installed.
-
-Optionally uses **msg.source** to set the source, if not already set in the properties.
-
 See node-example.json for more informations
 
+Copyright and license
+---------------------
+
+Copyright 2014 Nicholas Humfrey under [the Apache 2.0 license](LICENSE).
--- npro.js
+++ npro.js
@@ -14,182 +14,149 @@
  * limitations under the License.
  **/
 module.exports = function(RED) {
-	"use strict";
-    function NPNode(n) {
-        RED.nodes.createNode(this,n);
-        this.title = n.title || "";
-        this.source = n.source || "";
-        this.message = n.message || "";
-        this.priority =  Number( n.priority || 0);
-		this.apikey = n.apikey || undefined;
-		this.registrationId = n.registrationId || undefined;
-		var node = this;
-		this.on("input",function(msg) {
-			var message = msg.message || node.message || "";
-			var title = msg.title || node.title || "";
-			var priority = msg.priority || node.priority || 0;
-			var source = msg.source || node.source || 0;
-			var apikey = msg.apikey || node.apikey || undefined;
-			var registrationId = msg.registrationId || node.registrationId || undefined;
-			if (apikey == undefined || registrationId == undefined || title == undefined) {
-				this.error("No API key or registrationId or title set"); 
-			} else {
-				var message = {  
-					data: {
-						type: 'ntp_message',
-						message: new Buffer(message).toString('base64'),
-						priority:	priority,
-						title: new Buffer(title).toString('base64'),
-						source: new Buffer(source).toString('base64')
-					}
-				};
-				sendMessage(message, apikey, registrationId, function (err, data) {
-					if (err) {
-						node.warn("NP error: " + err);
-					}
-				});            
-			}
-		});
-    }
-    RED.nodes.registerType("npro",NPNode);
-}
+  "use strict";
+  function NPNode(n) {
+    RED.nodes.createNode(this,n);
+    this.title = n.title || "";
+    this.source = n.source || "";
+    this.message = n.message || "";
+    this.priority =  Number( n.priority || 0);
+    this.apikey = n.apikey || undefined;
+    this.registrationId = n.registrationId || undefined;
+    var node = this;
+    this.on("input",function(msg) {
+      var message = msg.message || node.message || "";
+      var title = msg.title || node.title || "";
+      var priority = msg.priority || node.priority || 0;
+      var source = msg.source || node.source || 0;
+      var apikey = msg.apikey || node.apikey || undefined;
+      var registrationId = msg.registrationId || node.registrationId || undefined;
+      if (apikey == undefined || registrationId == undefined || title == undefined) {
+        this.error("No APIkey, registrationId or title set"); 
+      } else {
+        var message = {
+          data: {
+            type: 'ntp_message',
+            message: new Buffer(message).toString('base64'),
+            priority:	priority,
+            title: new Buffer(title).toString('base64'),
+            source: new Buffer(source).toString('base64')
+          }
+        };  
+        sendMessage(message, apikey, registrationId, function (err, data) {
+          if (err) {
+            node.warn("NP error: " + err);
+          }
+        });
+      }
+    });
+  }
+  RED.nodes.registerType("npro",NPNode);
+};
 
 var Constants = {
-
-    'GCM_SEND_ENDPOINT' : 'android.googleapis.com',
-
-    'GCM_SEND_ENDPATH' : '/gcm/send',
-
-	'GCM_SEND_URI' : 'https://android.googleapis.com/gcm/send',
-
-    'PARAM_REGISTRATION_ID' : 'registration_id',
-
-    'PARAM_COLLAPSE_KEY' : 'collapse_key',
-
-    'PARAM_DELAY_WHILE_IDLE' : 'delay_while_idle',
-
-    'PARAM_PAYLOAD_KEY' : 'data',
-
-    'PARAM_TIME_TO_LIVE' : 'time_to_live',
-
-    'ERROR_QUOTA_EXCEEDED' : 'QuotaExceeded',
-
-    'ERROR_DEVICE_QUOTA_EXCEEDED' : 'DeviceQuotaExceeded',
-
-    'ERROR_MISSING_REGISTRATION' : 'MissingRegistration',
-
-    'ERROR_INVALID_REGISTRATION' : 'InvalidRegistration',
-
-    'ERROR_MISMATCH_SENDER_ID' : 'MismatchSenderId',
-
-    'ERROR_NOT_REGISTERED' : 'NotRegistered',
-
-    'ERROR_MESSAGE_TOO_BIG' : 'MessageTooBig',
-
-    'ERROR_MISSING_COLLAPSE_KEY' : 'MissingCollapseKey',
-
-    'ERROR_UNAVAILABLE' : 'Unavailable',
-
-    'TOKEN_MESSAGE_ID' : 'id',
-
-    'TOKEN_CANONICAL_REG_ID' : 'registration_id',
-
-    'TOKEN_ERROR' : 'Error',
-
-    'JSON_REGISTRATION_IDS' : 'registration_ids',
-
-    'JSON_PAYLOAD' : 'data',
-
-    'JSON_SUCCESS' : 'success',
-
-    'JSON_FAILURE' : 'failure',
-
-    'JSON_CANONICAL_IDS' : 'canonical_ids',
-
-    'JSON_MULTICAST_ID' : 'multicast_id',
-
-    'JSON_RESULTS' : 'results',
-
-    'JSON_ERROR' : 'error',
-
-    'JSON_MESSAGE_ID' : 'message_id',
-
-    'UTF8' : 'UTF-8',
-
-    'BACKOFF_INITIAL_DELAY' : 1000,
-
-    'MAX_BACKOFF_DELAY' : 1024000  ,
-
-    'SOCKET_TIMEOUT' : 180000 //three minutes
+  'GCM_SEND_ENDPOINT' : 'android.googleapis.com',
+  'GCM_SEND_ENDPATH' : '/gcm/send',
+  'GCM_SEND_URI' : 'https://android.googleapis.com/gcm/send',
+  'PARAM_REGISTRATION_ID' : 'registration_id',
+  'PARAM_COLLAPSE_KEY' : 'collapse_key',
+  'PARAM_DELAY_WHILE_IDLE' : 'delay_while_idle',
+  'PARAM_PAYLOAD_KEY' : 'data',
+  'PARAM_TIME_TO_LIVE' : 'time_to_live',
+  'ERROR_QUOTA_EXCEEDED' : 'QuotaExceeded',
+  'ERROR_DEVICE_QUOTA_EXCEEDED' : 'DeviceQuotaExceeded',
+  'ERROR_MISSING_REGISTRATION' : 'MissingRegistration',
+  'ERROR_INVALID_REGISTRATION' : 'InvalidRegistration',
+  'ERROR_MISMATCH_SENDER_ID' : 'MismatchSenderId',
+  'ERROR_NOT_REGISTERED' : 'NotRegistered',
+  'ERROR_MESSAGE_TOO_BIG' : 'MessageTooBig',
+  'ERROR_MISSING_COLLAPSE_KEY' : 'MissingCollapseKey',
+  'ERROR_UNAVAILABLE' : 'Unavailable',
+  'TOKEN_MESSAGE_ID' : 'id',
+  'TOKEN_CANONICAL_REG_ID' : 'registration_id',
+  'TOKEN_ERROR' : 'Error',
+  'JSON_REGISTRATION_IDS' : 'registration_ids',
+  'JSON_PAYLOAD' : 'data',
+  'JSON_SUCCESS' : 'success',
+  'JSON_FAILURE' : 'failure',
+  'JSON_CANONICAL_IDS' : 'canonical_ids',
+  'JSON_MULTICAST_ID' : 'multicast_id',
+  'JSON_RESULTS' : 'results',
+  'JSON_ERROR' : 'error',
+  'JSON_MESSAGE_ID' : 'message_id',
+  'UTF8' : 'UTF-8',
+  'BACKOFF_INITIAL_DELAY' : 1000,
+  'MAX_BACKOFF_DELAY' : 1024000  ,
+  'SOCKET_TIMEOUT' : 180000 //three minutes
 };
 
 var req = require('request');
-
 function sendMessage(message, senderKey, registrationId, callback) {
-    var body = {};
-    var requestBody;
-    var post_options;
-    var post_req;
-    var timeout;
-	var registrationIds = [];
-	registrationIds.push(registrationId);
-	
-    if (message.data === undefined) {
-		return callback(-1, 'message.data must be defined');
-	}
-	
-	if (message.data.timestamp === undefined) {
-		var now = new Date();
-		var timestampSplit = now.toISOString().split('.'); 
-		message.data.timestamp = timestampSplit[0];
+  var body = {};
+  var requestBody;
+  var post_options;
+  var post_req;
+  var timeout;
+  var registrationIds = [];
+  registrationIds.push(registrationId);
+  if (message.data === undefined) {
+    return callback(-1, 'message.data must be defined');
+  }
+
+  if (message.data.timestamp === undefined) {
+    var now = new Date();
+    var timestampSplit = now.toISOString().split('.'); 
+    message.data.timestamp = timestampSplit[0];
+  }
+  body[Constants.PARAM_PAYLOAD_KEY] = message.data;
+  body[Constants.JSON_REGISTRATION_IDS] = registrationIds;
+  requestBody = JSON.stringify(body);
+
+  post_options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-length': Buffer.byteLength(requestBody, 'utf8'),
+      'Authorization': 'key=' + senderKey
+    },
+    uri: Constants.GCM_SEND_URI,
+    body: requestBody
+  };
+
+  timeout = Constants.SOCKET_TIMEOUT;
+
+  post_options.timeout = timeout;
+
+  post_req = req(post_options, function (err, res, resBody) {
+    if (err) {
+      return callback(err, null);
     }
-	body[Constants.PARAM_PAYLOAD_KEY] = message.data;
-    body[Constants.JSON_REGISTRATION_IDS] = registrationIds;
-    requestBody = JSON.stringify(body);
-
-    post_options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-length': Buffer.byteLength(requestBody, 'utf8'),
-            'Authorization': 'key=' + senderKey
-        },
-        uri: Constants.GCM_SEND_URI,
-        body: requestBody
-    };
-
-    timeout = Constants.SOCKET_TIMEOUT;
-
-    post_options.timeout = timeout;
-
-    post_req = req(post_options, function (err, res, resBody) {
-
-        if (err)
-            return callback(err, null);
-
-        if (!res)
-            return callback('response is null', null);
-
-        if (res.statusCode === 503) {
-            console.log('GCM service is unavailable');
-            return callback(res.statusCode, null);
-        } else if(res.statusCode == 401){
-            console.log('Unauthorized');
-            return callback(res.statusCode, null);
-        } else if (res.statusCode !== 200) {
-            console.log('Invalid request: ' + res.statusCode);
-            return callback(res.statusCode, null);
+    if (!res) {
+      return callback('response is null', null);
+    }
+    if (res.statusCode === 503) {
+      console.log('GCM service is unavailable');
+      return callback(res.statusCode, null);
+    } else {
+      if(res.statusCode == 401) {
+        console.log('Unauthorized');
+        return callback(res.statusCode, null);
+      } else {
+        if (res.statusCode !== 200) {
+          console.log('Invalid request: ' + res.statusCode);
+          return callback(res.statusCode, null);
         }
+      }
+    }
 
-        // Make sure that we don't crash in case something goes wrong while
-        // handling the response.
-        try {
-            var data = JSON.parse(resBody);
-			callback(null, data);
-        } catch (e) {
-            console.log("Error handling GCM response " + e);
-            return callback("error", null);
-        }
-    });
+    // Make sure that we don't crash in case something goes wrong while
+    // handling the response.
+    try {
+      var data = JSON.parse(resBody);
+      callback(null, data);
+    } catch (e) {
+      console.log("Error handling GCM response " + e);
+      return callback("error", null);
+    }
+  });
 }
-
